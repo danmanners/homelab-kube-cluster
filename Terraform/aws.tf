e191becd3f1fa7cb6362bdc2d1a1b3370da7beb2
Dan Manners <daniel.a.manners@gmail.com>
2021-07-28 23:03:51 -0400

    Updated Terraform to remove GCP and add AWS.

---
+++ Terraform/aws.tf
@@ -0,0 +1,57 @@
+#########################################################################
+### Networking
+
+// Create the cloud network stack
+module "aws_vpc" {
+  // Module Source
+  source      = "git::https://github.com/danmanners/GCP-Learning.git//Single-Node-Multi-Provider/modules/aws/vpc"
+
+  // Networking Settings
+  cidr_block  = var.aws.vpc.cidr_block
+  subnets     = var.aws.subnets
+  tags        = var.aws.tags
+}
+
+#########################################################################
+### Virtual Machines
+
+// Create the AWS EC2 Instances
+module "aws_compute" {
+  // Module Source
+  source      = "git::https://github.com/danmanners/GCP-Learning.git//Single-Node-Multi-Provider/modules/aws/ec2"
+  // Compute Settings
+  compute_nodes   = var.aws.compute
+  public_subnets  = module.aws_vpc.public_subnets
+  ssh_auth        = var.ssh_auth
+  tags            = var.aws.tags
+
+  // Depends On the AWS VPC being ready
+  depends_on      = [
+    module.aws_vpc
+  ]
+}
+
+#########################################################################
+### Security Groups
+
+module "aws_k3s_security_groups" {
+  // Module Source
+  source      = "git::https://github.com/danmanners/GCP-Learning.git//Single-Node-Multi-Provider/modules/aws/security_group"
+  vpc_id = module.aws_vpc.vpc_id
+  security_groups = var.aws.security_groups
+  tags = var.aws.tags
+}
+
+module "aws_k3s_security_group_association" {
+  // Module Source
+  source      = "git::https://github.com/danmanners/GCP-Learning.git//Single-Node-Multi-Provider/modules/aws/ec2_sg_associate"
+
+  // Load in Security Groups and ENIs
+  security_groups = module.aws_k3s_security_groups.security_group_ids
+  ec2_enis        = module.aws_compute.primary_net_interface_id
+
+  depends_on = [
+    module.aws_compute,
+    module.aws_k3s_security_groups
+  ]
+}
--- Terraform/global_inputs.tf
+++ Terraform/global_inputs.tf
@@ -1,5 +1,9 @@
-variable "google_cloud_auth" {
-  description = ""
+variable "cloud_auth" {
+  description = "Multiple values for cloud authentication."
+}
+
+variable "aws" {
+  description = "All AWS specific resource values should be loaded in this top-level map."
 }
 
 variable "ssh_auth" {
