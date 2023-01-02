e674a2d8fe710fdd4f64ce548e7096daa0769288
Dan Manners <daniel.a.manners@gmail.com>
2023-01-01 21:47:50 -0500

    Functional so far!

--- infrastructure-provisioning/cloud/pulumi/types/types.ts
+++ infrastructure-provisioning/cloud/pulumi/types/types.ts
@@ -1,18 +1,18 @@
 import * as pulumi from "@pulumi/pulumi"
 
-export type NetworkingPreview = {
-  // Creating VPC
-  vpc: {
-    arn: pulumi.Output<string>
-    id: pulumi.Output<string>
-    name: pulumi.Output<string>
+export type NameIdOutputs = {
+  [name: string]: {
+    id: pulumi.Output<string>,
+    routeTableRoute?: pulumi.Output<string>,
   }
-  // Public Subnets
-  public_subnets: { [name: string]: { id: pulumi.Input<string> } }
-  // Private Subnets
-  private_subnets: { [name: string]: { id: pulumi.Input<string> } }
 }
 
+// Route Table Layouts
+export type RouteTableIds = {
+  [name: string]: pulumi.Output<string>
+}
+
+// Typeing for Compute Nodes
 export type Node = {
   name: string
   instance_size: string
