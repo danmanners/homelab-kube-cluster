c3e9c3c8e0a73b9a650a2e4fe1c0580679142eac
Dan Manners <daniel.a.manners@gmail.com>
2023-02-18 14:34:08 -0500

    Deleting unnecessary code

--- infrastructure-provisioning/cloud/terraform/00-infrastructure/vpc.tf
+++
@@ -1,8 +0,0 @@
-module "aws_vpc" {
-  source = "../modules/vpc"
-
-  vpc_cidr_block  = var.network.vpc.cidr_block
-  public_subnets  = var.network.subnets.public
-  private_subnets = var.network.subnets.private
-  tags            = var.tags
-}
--- infrastructure-provisioning/cloud/terraform/modules/vpc/vpc.tf
+++
@@ -1,5 +0,0 @@
-// Create the VPC
-resource "aws_vpc" "primary" {
-  cidr_block = var.vpc_cidr_block
-  tags       = var.tags
-}
